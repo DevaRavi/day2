566f0ac4379761a36593cfd824f6ddcb187e7523
DevaRavi <reltecdeva@gmail.com>
2022-01-20 22:46:00 +0530

    username

--- src/test/java/org/test/Git2.java
+++ src/test/java/org/test/Git2.java
@@ -1,5 +1,12 @@
 package org.test;
 
 public class Git2 {
+	public static void username() {
+		System.out.println("username");
+
+	}
+	public static void main(String[] args) {
+		username();
+	}
 
 }
